76ac65e921bc0342d33021127dbebebb44269b88
symonmuchemi <simonkenya846@gmail.com>
2025-02-03 15:56:12 +0300

    chore: correct code to ensure PEP8 compliance

--- server.py
+++ server.py
@@ -5,74 +5,80 @@ import socket
 import concurrent.futures
 from utils import parse_config_file
 
-def handle_client(client_socket, address, payload_size = 1024) -> None:
+
+def handle_client(client_socket, address, payload_size=1024) -> None:
     print(f"Connection established with: {address}")
-    
+
     try:
         while True:
             data = client_socket.recv(payload_size)
             if not data:
                 print("No data reveived!")
                 break
-            print(f'Data received: {data}')
-            server_response = b'Message received'
-            client_socket.sendall(server_response)
+            print(f"Data received: {data}")
+            response = b"Message received"
+            client_socket.sendall(response)
     except BrokenPipeError:
-        print(f'Error: Broken pipe when sending data to {address}')
+        print(f"Error: Broken pipe when sending data to {address}")
     except Exception as e:
         print(f"Error starting the server: {e}")
     finally:
         client_socket.close()
         print(f"Connection with {address} closed")
 
+
 def start_server() -> None:
     try:
         configs = parse_config_file("./config/config.txt")
-        
+
         if not configs:
-            print('Server configurations missing')
+            print("Server configurations missing")
             return
-        
+
         server_address: tuple = (configs.get("HOST"), configs.get("PORT"))
-        PAYLOAD_SIZE = configs.get('PAYLOAD_SIZE')
-        
+        PAYLOAD_SIZE = configs.get("PAYLOAD_SIZE")
+
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind(server_address)
             s.listen()
-            
+
             while True:
                 conn, addr = s.accept()
-                
+
                 handle_client(conn, addr, PAYLOAD_SIZE)
     except Exception as e:
         print(f"Error starting the server: {e}")
 
+
 def start_server_with_threading() -> None:
     try:
         configs = parse_config_file("./config/config.txt")
-        
+
         if not configs:
-            print('Server configurations missing')
+            print("Server configurations missing")
             return
-        
+
         server_address: tuple = (configs.get("HOST"), configs.get("PORT"))
-        PAYLOAD_SIZE = configs.get('PAYLOAD_SIZE')
-        
+        PAYLOAD_SIZE = configs.get("PAYLOAD_SIZE")
+
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
+
         server_socket.bind(server_address)
         server_socket.listen()
-        
-        print('Listening for connections')
-        
+
+        print("Listening for connections")
+
         with concurrent.futures.ThreadPoolExecutor() as executor:
             while True:
-                print("Waiting for a client to connect")
+                print("Waiting for a client to connect...")
                 client_socket, client_address = server_socket.accept()
-                executor.submit(handle_client, client_socket, client_address, PAYLOAD_SIZE)
-    
+                executor.submit(
+                    handle_client, client_socket, client_address, PAYLOAD_SIZE
+                )
+
     except Exception as e:
         print(f"Error starting the server: {e}")
+
 
 if __name__ == "__main__":
     # start_server()
--- utils.py
+++ utils.py
@@ -1,26 +1,27 @@
 #!/bin/env python3
 """ utilities """
 
+
 def parse_config_file(file_path: str) -> dict | None:
     configurations = {}
-    
+
     try:
         with open(file_path, 'r') as file:
             for line in file:
                 line = line.strip()
-                
+
                 key, val = line.split('=', 1)
-                
+
                 if key in ['PORT', 'PAYLOAD_SIZE']:
                     val = int(val)
-                
+
                 configurations[key] = val
-            
+
             print(f'Server configuration dictionary: {configurations}')
             return configurations
     except FileNotFoundError:
         print(f"Error: Configuration file '{file_path}' not found.")
     except Exception as e:
-        print(f"Error: An unexpected error occurred while parsing the configuration file: {e}")
-    
+        print(f"Error parsing the configuration file: {e}")
+
     return None
