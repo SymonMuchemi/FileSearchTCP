62d4bf566778a43f500ca448249871cac9b80518
symonmuchemi <simonkenya846@gmail.com>
2025-02-03 15:05:30 +0300

    feat: Enhance client-server communication with threading support and improved error handling

--- client.py
+++ client.py
@@ -13,19 +13,21 @@ def start_client() -> None:
         print('Server configurations missing')
         return
     
-    host_and_port_tuple= (configs.get("HOST"), configs.get("PORT"))
+    server_address = (configs.get("HOST"), configs.get("PORT"))
     PAYLOAD_SIZE = (configs.get('PAYLOAD_SIZE'))
     
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(host_and_port_tuple)
-            s.sendall(bytes(sys.argv[1], 'utf-8'))
-            
-            data = s.recv(PAYLOAD_SIZE)
-            
-            print(f"Server response: {data}")
+            s.connect(server_address)
+            while True:
+                s.send(bytes(sys.argv[1], 'utf-8'))
+                
+                data = s.recv(PAYLOAD_SIZE)
+                
+                print(f"Server response: {data}")
+
     except Exception as e:
-        print(f"Error starting the server: {e}")
+        print(f"Error starting the client: {e}")
 
 if __name__ == '__main__':
     start_client()    
--- server.py
+++ server.py
@@ -2,16 +2,23 @@
 """ Server """
 
 import socket
+import concurrent.futures
 from utils import parse_config_file
 
-def handle_client(client_socket, address) -> None:
+def handle_client(client_socket, address, payload_size = 1024) -> None:
     print(f"Connection established with: {address}")
     
     try:
-        data = client_socket.recv(1024) # TODO: use variable
-        print(f'Data received: {data}')
-        server_response = b'Message received'
-        client_socket.sendall(server_response)
+        while True:
+            data = client_socket.recv(payload_size)
+            if not data:
+                print("No data reveived!")
+                break
+            print(f'Data received: {data}')
+            server_response = b'Message received'
+            client_socket.sendall(server_response)
+    except BrokenPipeError:
+        print(f'Error: Broken pipe when sending data to {address}')
     except Exception as e:
         print(f"Error starting the server: {e}")
     finally:
@@ -26,19 +33,47 @@ def start_server() -> None:
             print('Server configurations missing')
             return
         
-        host_and_port_tuple: tuple = (configs.get("HOST"), configs.get("PORT"))
+        server_address: tuple = (configs.get("HOST"), configs.get("PORT"))
         PAYLOAD_SIZE = configs.get('PAYLOAD_SIZE')
         
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.bind(host_and_port_tuple)
+            s.bind(server_address)
             s.listen()
             
             while True:
                 conn, addr = s.accept()
                 
-                handle_client(conn, addr)
+                handle_client(conn, addr, PAYLOAD_SIZE)
+    except Exception as e:
+        print(f"Error starting the server: {e}")
+
+def start_server_with_threading() -> None:
+    try:
+        configs = parse_config_file("./config/config.txt")
+        
+        if not configs:
+            print('Server configurations missing')
+            return
+        
+        server_address: tuple = (configs.get("HOST"), configs.get("PORT"))
+        PAYLOAD_SIZE = configs.get('PAYLOAD_SIZE')
+        
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        server_socket.bind(server_address)
+        server_socket.listen()
+        
+        print('Listening for connections')
+        
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            while True:
+                print("Waiting for a client to connect")
+                client_socket, client_address = server_socket.accept()
+                executor.submit(handle_client, client_socket, client_address, PAYLOAD_SIZE)
+    
     except Exception as e:
         print(f"Error starting the server: {e}")
 
 if __name__ == "__main__":
-    start_server()
+    # start_server()
+    start_server_with_threading()
